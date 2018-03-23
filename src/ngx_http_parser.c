37f0797f44db1fa649a0e8c8e1bec65a06ace3b0
gfrankliu <gfrankliu@gmail.com>
2018-03-22 19:48:49 -0700

    Fixing GCC7 compiling errors (#203)
    * Rename function ngx_strnlen to avoid conflict with nginx 1.12.2+
    
    * Fixing https://github.com/weibocom/nginx-upsync-module/issues/189
    
    * Revert the change to add passing query param.
    
    This reverts commit ba59a7899636713a34cccf538304dfb420169f16.
    
    * Fixing https://github.com/weibocom/nginx-upsync-module/issues/196
    
    * Fixing https://github.com/weibocom/nginx-upsync-module/issues/202

--- src/ngx_http_parser.c
+++ src/ngx_http_parser.c
@@ -2327,8 +2327,8 @@ http_parser_parse_url(const char *buf, size_t buflen, int is_connect,
 
       case s_req_server_with_at:
         found_at = 1;
+        /* FALLTHRU */
 
-      /* FALLTROUGH */
       case s_req_server:
         uf = UF_HOST;
         break;
