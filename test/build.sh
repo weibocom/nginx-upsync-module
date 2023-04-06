3fede3b50c3074555bf4fff4f7cc315d3e682b2a
msaf1980 <msaf1980@gmail.com>
2023-04-06 23:23:34 +0500

    tests: fix tests and add tests in ci

---
+++ test/build.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+if [ ! -d "_nginx" ]; then
+    echo "run: ./fetch NGINX_VERSION" >&2
+    exit 1
+fi
+
+cd _nginx || exit 1
+
+exec make
---
+++ test/fetch.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+if [ -d "_nginx" ]; then
+    exit
+fi
+
+wget -q http://nginx.org/download/nginx-${1}.tar.gz || exit 1
+tar -xzf nginx-${1}.tar.gz || exit 1
+exec mv  nginx-${1} _nginx
